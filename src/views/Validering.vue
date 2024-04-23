7e25a58d3fbd6e25aa6443a2cbb7f22f2e22c677
Magnus Bomholt <hello@magnusgb.dk>
2024-04-23 21:25:42 +0200

    Oprydning i vue fil, klar til udvikling

---
+++ src/views/Validering.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+import Valideringsform from '../components/Valideringsform.vue'
+</script>
+
+<template>
+  <main>
+    <Valideringsform />
+  </main>
+</template>
